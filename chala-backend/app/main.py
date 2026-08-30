0e07ff4b0f0257c90d12e51e9b228f76e0ae3299
Chalani Nisansala <chalani.nisansala201@gmail.com>
2026-08-30 23:26:06 +0530

    Update main.py

--- chala-backend/app/main.py
+++ chala-backend/app/main.py
@@ -44,6 +44,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5000",
         "http://127.0.0.1:5000",
+        "https://r26-it-052.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
